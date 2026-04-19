8be884aba28b75be943a74e9d76adc3bd97490fa
nwn900 <nwn900@users.noreply.github.com>
2026-04-19 22:57:46 +0200

    Fix Google Login: Spoof User-Agent

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -46,6 +46,7 @@ fn main() {
                 WebviewUrl::External(gemini_url),
             )
             .title("Gemini")
+            .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
             .inner_size(1200.0, 900.0)
             .auto_resize()
             .on_navigation(|url| {
