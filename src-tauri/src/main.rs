8da06687e939a73b061dcc7ef522dff2a6011173
nwn900 <nwn900@users.noreply.github.com>
2026-04-19 23:09:31 +0200

    Fix Google Login: Use Firefox UA

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -46,7 +46,7 @@ fn main() {
                 WebviewUrl::External(gemini_url),
             )
             .title("Gemini")
-            .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
+            .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0")
             .inner_size(1200.0, 900.0)
             .auto_resize()
             .on_navigation(|url| {
